e45a181fce8cf2cb670323ae0505773512e54c2a
Jim Kang <jimkang@gmail.com>
2015-08-05 08:46:49 -0400

    Updated getActivitiesFromHTML to derive ids for activities from the comment links.

--- tests/get-activities-from-html-tests.js
+++ tests/get-activities-from-html-tests.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var jsonfile = require('jsonfile');
 var restoreDate = require('./fixtures/restore-date');
 
+var validIdRegex = /^\w+-\d+-\d+$/;
+
 var userActivityHTML = fs.readFileSync(
   __dirname + '/fixtures/user-activity.html',
   {
@@ -18,7 +20,7 @@ var expectedActivities = jsonfile.readFileSync(
 expectedActivities = expectedActivities.map(restoreDate);
 
 test('Parse test', function parseTest(t) {
-  t.plan(expectedActivities.length * 2);
+  t.plan(expectedActivities.length * 3);
 
   var activities = getActivitiesFromHTML(userActivityHTML);
   activities.forEach(checkActivity);
@@ -32,5 +34,6 @@ test('Parse test', function parseTest(t) {
     t.equal(
       activity.html, expectedActivities[i].html, 'Parses activity html.'
     );
+    t.ok(activity.id.match(validIdRegex), 'Activity has an proper id.');
   }
 });

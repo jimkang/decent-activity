18ab46ea90a4c0c2bb74e4114411bd667c41de4f
Jim Kang <jimkang@gmail.com>
2015-08-06 08:46:19 -0400

    Added tests to ensure sure that the rendered DOM matches the data. Implemented enter and exit in renderActivities.

--- tests/render-activities-tests.js
+++ tests/render-activities-tests.js
@@ -19,6 +19,10 @@ function findActivityForStamp(stamp) {
   }
 }
 
+function aStampIsOlderThanBStamp(a, b) {
+  return a.stamp < b.stamp;
+}
+
 test('Render activities under the root by date', function renderTest(t) {
   var document = jsdom.jsdom(
     '<div id="outer">' +
@@ -42,7 +46,11 @@ test('Render activities under the root by date', function renderTest(t) {
   });
 
   var activityEls = body.selectAll('.activity');
-  t.equal(activityEls.size(), 5, 'Renders an element for each activity.');
+  t.equal(
+    activityEls.size(),
+    activities.length,
+    'Renders an element for each activity.'
+  );
 
   var lastStampChecked = new Date();
   activityEls.each(checkActivityEl);
@@ -67,4 +75,56 @@ test('Render activities under the root by date', function renderTest(t) {
   t.end();
 });
 
-// TODO: Put in its own module.
+test('Rendered DOM matches data', function enterExitTest(t) {
+  var document = jsdom.jsdom(
+    '<div id="outer">' +
+      'Hi!' +
+      '<div id="inner"></div>' +
+      '<span id="other">That\'s the activities!</span>' +
+    '</div>'
+  );
+
+  var body = d3.select(document.body);
+  var root = body.select('#inner').node();
+
+  var renderActivities = createRenderActivities({
+    d3: d3,
+    activityClass: 'activity'
+  });
+
+  var activityGroup1 = activities.slice(0, 3);
+  var activityGroup2 = activities.slice(2, 4);
+  var activityGroup3 = activities.slice(4);
+
+  [activityGroup1, activityGroup2, activityGroup3].forEach(testGroup);
+
+  function testGroup(activityGroup) {
+    renderActivities({
+      root: root,
+      activities: activityGroup
+    });
+
+    var activityEls = body.selectAll('.activity');
+
+    t.equal(
+      activityEls.size(),
+      activityGroup.length,
+      'Renders an element for each activity.'
+    );
+
+    activityGroup = activityGroup.sort(aStampIsOlderThanBStamp);
+    activityEls.each(checkActivityEl);
+
+    function checkActivityEl(d, i) {
+      var activityAtPosition = activityGroup[i];
+      t.equal(
+        d.id, activityAtPosition.id, 'Activity is rendered in correct order.'
+      );
+      t.equal(
+        d.html, activityAtPosition.html, 'HTML is correct for activity.'
+      );
+    }
+  }
+
+  t.end();
+});
